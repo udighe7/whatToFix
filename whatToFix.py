d81b39460685c42d6cb32e3ce166ac0ad2f1d4c3
UtkarshDighe <utkarsh@helpshift.com>
2020-09-14 08:38:37 +0530

    Add data to final HTML file generated

--- whatToFix.py
+++ whatToFix.py
@@ -1,19 +1,53 @@
 import subprocess
 import re
 import csv
+import os
+import pandas as pd
+import collections
 
 testNames = ['HelpshiftDemoiOSUIConvTests','HelpshiftDemoiOSUIFormTests']
-
-def getStartingHTMLString():
-    startingHTMLString = "<!DOCTYPE html><html lang='en' ><head><meta charset='UTF-8'><title>WHAT-TO-FIX</title><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'><link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Titillium+Web:300,700,300italic'><link rel='stylesheet' href='./style.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js'></script></head><body>"
-    return startingHTMLString
 
 def getEndingHTMLString():
     endingHTMLString = "</body></html>"
     return endingHTMLString
 
+def getHTMLStringFromCSV(csvFile):
+    df = pd.read_csv(csvFile)
+    failureReasons = df.groupby('reason').groups.keys()
+    htmlstring = ""
+    if len(failureReasons):
+        dictToCount = {}
+        for reason in failureReasons:
+            dictToCount[reason] = df[df['reason'] == reason]['testCase'].nunique()
+        od = collections.OrderedDict(sorted(dictToCount.items(), key=lambda x: x[1], reverse=True))
+        htmlstring = "<br><h2 text-transform: uppercase>"+csvFile+"</h2>"
+        htmlstring += "<ul>"
+        for reason in od.keys():
+            htmlstring += "<li><input type='checkbox' checked><i></i><h2>"
+            htmlstring += reason
+            htmlstring += "</h2><p>"
+            testCases = df[df['reason'] == reason]['testCase'].values.tolist()
+            for testCase in testCases:
+                htmlstring += testCase
+                htmlstring += "<br>"
+            htmlstring += "</p></li>"
+        htmlstring += "</ul>"
+    return htmlstring
+
+def getMiddleHTMLString():
+    csvFiles = [f for f in os.listdir(os.getcwd()) if f.endswith('.csv')]
+    middleHTMLString = ""
+    for csvFile in csvFiles:
+        middleHTMLString += getHTMLStringFromCSV(csvFile)
+    return middleHTMLString
+
+def getStartingHTMLString():
+    startingHTMLString = "<!DOCTYPE html><html lang='en' ><head><meta charset='UTF-8'><title>WHAT-TO-FIX</title><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'><link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Titillium+Web:300,700,300italic'><link rel='stylesheet' href='./style.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js'></script></head><body><h1>What-To-Fix</h1>"
+    return startingHTMLString
+
 def createHTMLFile():
     htmlString = getStartingHTMLString()
+    htmlString += getMiddleHTMLString()
     htmlString += getEndingHTMLString()
     with open("whatToFix.html", "w") as text_file:
         text_file.write(htmlString)
